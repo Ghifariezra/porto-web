24ed0143978c2d45128774af04d60d8efa10b78e
Owner <ghifariezraramadhan@gmail.com>
2025-06-02 09:38:16 +0700

    update home page

--- app/section/home/project.tsx
+++ app/section/home/project.tsx
@@ -16,10 +16,10 @@ export default function Projects() {
             className="flex flex-col w-full h-fit gap-4 px-6 pt-6 bg-white/90 dark:bg-white/5 border border-zinc-200 dark:border-white/10 backdrop-blur-lg rounded-2xl overflow-hidden group hover:shadow-xl hover:shadow-cyan-200/40 dark:hover:shadow-cyan-400/20 transition-transform duration-300 hover:scale-[1.02]"
           >
             <div className="flex w-full justify-center items-center gap-2">
-              <h1 className="font-bold text-center text-sm sm:text-lg px-3 py-1 rounded-full w-fit bg-zinc-800 dark:bg-zinc-100 text-zinc-100 dark:text-zinc-800">{item.status}</h1>
+              <h1 className="font-bold text-center text-sm sm:text-lg px-3 py-1 rounded-full w-full bg-zinc-800 dark:bg-transparent dark:border-zinc-100 dark:border text-zinc-100">{item.status}</h1>
             </div>
             <div className="flex justify-between items-center gap-2">
-              <h1 className="font-semibold text-[11px] sm:text-base w-full px-3 py-1 rounded-full border dark:border-none dark:bg-white text-zinc-800">{item.title}</h1>
+              <h1 className="font-semibold text-[11px] sm:text-base w-fit px-3 py-1 rounded-full border dark:border-none dark:bg-white text-zinc-800">{item.title}</h1>
               <h2 className="font-semibold text-[11px] sm:text-base px-3 py-1 rounded-full border dark:border-none dark:bg-white text-zinc-800">{item.years}</h2>
             </div>
             <div className="flex flex-col gap-4">
