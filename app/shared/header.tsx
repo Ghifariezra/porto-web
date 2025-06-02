eb5682be3c1fed72de4c2d78149d993683f1e896
Owner <ghifariezraramadhan@gmail.com>
2025-06-02 09:42:21 +0700

    update home page

--- app/shared/header.tsx
+++ app/shared/header.tsx
@@ -8,14 +8,14 @@ export default function Header() {
 
   if (pathInteraction.includes(pathname)) {
     return (
-      <header className="sticky inset-2 flex flex-col justify-between gap-2 items-center p-4">
+      <header className="sticky z-50 inset-2 flex flex-col justify-between gap-2 items-center p-4 h-fit">
         <Navbar />
       </header>
     );
   }
 
   return (
-    <header className="inset-2 z-80 flex flex-col justify-start gap-2 items-center p-4 fixed h-fit">
+    <header className="inset-2 z-80 flex flex-col justify-start gap-2 items-center p-2 fixed h-fit">
       <Navbar />
     </header>
   );
