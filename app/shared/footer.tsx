777ca2b8b33aaca2b383bd3744fac774bfe7b165
Owner <ghifariezraramadhan@gmail.com>
2025-06-03 14:21:30 +0700

    add pointing section

--- app/shared/footer.tsx
+++ app/shared/footer.tsx
@@ -2,7 +2,7 @@ import Contact from "@/app/section/contact";
 
 export default function Footer() {
   return (
-    <footer className="p-4">
+    <footer id="contact" className="p-4">
       <Contact />
     </footer>
   );
