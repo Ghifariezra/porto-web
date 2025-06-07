899234ef296971f199d35d3f5ab9d018d06317ba
Owner <ghifariezraramadhan@gmail.com>
2025-06-07 11:00:39 +0700

    add auth

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prisma"  // ✅ tambahkan ini
+  ),
 ];
 
 export default eslintConfig;
