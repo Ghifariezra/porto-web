8b13f8c205a4999353013d72ff4e3b9c40532d5a
Owner <ghifariezraramadhan@gmail.com>
2025-06-09 06:37:55 +0700

    add feature new post for admin

--- app/api/(crud)/add/route.tsx
+++ app/api/(crud)/add/route.tsx
@@ -7,11 +7,9 @@ const secret = new TextEncoder().encode(process.env.JWT_SECRET || "supersecret")
 export async function POST(req: Request) {
   try {
     // Ambil cookie dari request
-    // Output: __next_hmr_refresh_hash__=330; token=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJwM0wzcnUjIiwiaWF0IjoxNzQ5NDIzNDg5LCJleHAiOjE3NDk0MjcwODl9.iEUZJ12epOuUG5jT8Oyr0l35SOcUyE5io0CcBUwR6k
     const cookieHeader = req.headers.get("cookie");
 
     // Ambil token dari cookie
-    // Output: eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJwM0wzcnUjIiwiaWF0IjoxNzQ5NDIzNDg5LCJleHAiOjE3NDk0MjcwODl9.iEUZJ12epOuUG5jT8Oyr0l35SOcUyE5io0CcBUwR6k8
     const token = cookieHeader
       ?.split(";")
       .find((c) => c.trim().startsWith("token="))
@@ -22,7 +20,6 @@ export async function POST(req: Request) {
     }
 
     // Verifikasi token dan dapatkan payload-nya
-    // Output: { id: 1, username: 'p3L3ru#', iat: 1749423489, exp: 1749427089 }
     const { payload } = await jwtVerify(token, secret);
     console.log(payload);
 
